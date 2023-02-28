dcc86df2ee9ed17cb139455681a4a513981ed009
Bigkenny12w <bigkenny12w@gmail.com>
2023-02-28 03:46:03 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Printing a program with the function puts
- * Return - 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	puts("\n");
 	return (0);
 }
